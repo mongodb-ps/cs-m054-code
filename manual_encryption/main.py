94faa5b89bd449bada62b83d5280493af15073de
beergeek <beergeek1679@gmail.com>
2024-12-10 11:38:08 +1100

    update Golang to make code nicer

--- manual_encryption/main.py
+++ manual_encryption/main.py
@@ -105,7 +105,7 @@ def main():
     kms_tls_options = {
       "kmip": {
         "tlsCAFile": "/data/pki/ca.pem",
-        "tlsCertificateKeyFile": "/data/pki/ca.pem"
+        "tlsCertificateKeyFile": "/data/pki/client-0.pem"
       }
     }
   )
